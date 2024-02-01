967cea909e754f1995e59084e162bd6b269fe67a
ykcaj-i <jliuarc@gmail.com>
2024-02-01 00:32:57 -0500

    Delete my website directory

--- my website/script.js
+++
@@ -1,8 +0,0 @@
-
-
-function myFunction() {
-   var element = document.body;
-   var element1 = document.username;
-   element.classList.toggle("dark-mode");
-   element1.classList.toggle("dark-mode");
-}
