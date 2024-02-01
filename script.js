2ac93a4c97595c5c7111514c54cc7fc727eebfe0
ykcaj-i <jliuarc@gmail.com>
2024-02-01 00:33:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function myFunction() {
+   var element = document.body;
+   var element1 = document.username;
+   element.classList.toggle("dark-mode");
+   element1.classList.toggle("dark-mode");
+}
