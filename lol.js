5102194d1eedaeee9fc698060c77d4e563e11fbe
ykcaj-i <jliuarc@gmail.com>
2024-02-02 15:19:19 -0500

    updated navbar!

---
+++ lol.js
@@ -0,0 +1,24 @@
+
+/*toggle dark mode*/
+function myFunction() {
+   var element = document.body;
+   var element1 = document.username;
+   element.classList.toggle("dark-mode");
+   element1.classList.toggle("dark-mode");
+}
+/*sticky navbar*/
+
+
+
+
+
+
+
+
+function myFunction2(){
+
+}
+
+function myFunction3(){
+
+}
